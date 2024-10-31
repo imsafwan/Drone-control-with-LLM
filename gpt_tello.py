bbdc919b3b523f62a255b8a37b4d4912ab5058ce
Mondal <mmonda4@uic.edu>
2024-10-31 12:52:50 -0500

    corrected

--- gpt_tello.py
+++ gpt_tello.py
@@ -6,8 +6,6 @@ Created on Wed Oct 30 10:56:30 2024
 """
 
 import sys
-print(sys.executable)
-sys.path.append(r'c:\users\giuseppe\appdata\local\packages\pythonsoftwarefoundation.python.3.10_qbz5n2kfra8p0\localcache\local-packages\python310\site-packages')
 
 
 from djitellopy import Tello
@@ -79,7 +77,8 @@ if confirm == 'y':
         else:
             print(f"Warning: No executable skill found for '{action}'")  
 else:
-     print(f"Skipped action ")
+     print(f"Skipped action git init
+           ")
 
 # End session
 drone.end()
